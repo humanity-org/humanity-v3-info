ec0c51907f4aa8d5a4262c39331d139dca3ff277
Den <dzianis.smalonski@protofire.io>
2025-09-30 12:47:27 +0200

    fix: correct GRAPH_NODE_URL to use the production endpoint

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-export const GRAPH_NODE_URL = process.env.REACT_APP_GRAPH_NODE_URL ?? `https://graph.staging.swap.humanity.org`
+export const GRAPH_NODE_URL = process.env.REACT_APP_GRAPH_NODE_URL ?? `https://graph.swap.humanity.org`
 
 export const healthClient = new ApolloClient({
   uri: 'https://api.thegraph.com/index-node/graphql',
